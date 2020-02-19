79fbd2751da4ae78bae168677707860a481fb41a
Solomakhin Anton <s.anton45@yandex.ru>
2020-02-19 23:09:23 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketKopecks = 755000;
+        int miles = ticketKopecks/20/100;
+        System.out.println(miles);
+    }
+}
